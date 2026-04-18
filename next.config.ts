4fdd91223404bf50589da4358c70ab8d9524b61e
Qofy <safopencilart@icloud.com>
2026-04-18 11:07:26 +0200

    feat(profile): implement profile image upload and update functionality

--- next.config.ts
+++ next.config.ts
@@ -6,9 +6,9 @@ const nextConfig: NextConfig = {
     // Allow loading images from Unsplash (used by your movie posters)
     domains: ["images.unsplash.com"],
     // Alternatively you can use `remotePatterns` for more control:
-    // remotePatterns: [
-    //   { protocol: 'https', hostname: 'images.unsplash.com', pathname: '/**' }
-    // ]
+     remotePatterns: [
+      {hostname:"res.cloudinary.com"}
+     ]
   },
 };
 
